4d166ae7383ac27771ba5b0a32cf1efb5cbfce09
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-18 19:18:46 -0200

    Changes on firstIonicApp and adds two new apps (ionicTabsApp and EliteScheduleApp)

---
+++ pluralsight_building_mobile_apps_ionic_angularjs/EliteScheduleApp/www/app/home/leagues-ctrl.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular.module('eliteApp').controller('LeaguesCtrl', ['$state', 'eliteApi', LeaguesCtrl]);
+
+    function LeaguesCtrl($state, eliteApi) {
+        var vm = this;
+
+        vm.leagues = eliteApi.getLeagues();
+
+        vm.selectLeague = function(leagueId) {
+            //TODO: select correct league
+            $state.go("app.teams");
+        };
+    };
+})();
