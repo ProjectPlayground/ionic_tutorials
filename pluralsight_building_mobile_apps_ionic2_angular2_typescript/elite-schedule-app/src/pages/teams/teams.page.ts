0a3908e77e892f9611dcad24a5d91a9d0b0ba057
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-26 16:02:58 -0200

    PluralSight - Ionic2 - EliteSchedule - adds navigation to TeamDetail, Teams and Tournaments pages

--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/teams/teams.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/teams/teams.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { TeamDetailPage } from '../pages';
+
 @Component({
   templateUrl: 'teams.page.html'
 })
@@ -9,4 +11,7 @@ export class TeamsPage {
   constructor(private nav: NavController) {
   }
 
+  itemTapped() {
+    this.nav.push(TeamDetailPage);
+  }
 }
--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/tournaments/tournaments.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/tournaments/tournaments.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { TeamsPage } from '../pages';
+
 @Component({
   templateUrl: 'tournaments.page.html'
 })
@@ -9,7 +11,7 @@ export class TournamentsPage {
   constructor(private nav: NavController) {
   }
 
-  navigate() {
-    this.nav.pop();
+  itemTapped() {
+    this.nav.push(TeamsPage);
   }
 }
