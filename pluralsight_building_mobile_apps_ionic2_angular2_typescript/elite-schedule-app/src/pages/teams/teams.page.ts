e32bdb5325f6ab4cbd83542d527e63a0391e040b
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-27 17:34:03 -0200

    PluralSight - Ionic2 - EliteSchedule - Teams - adds styling with list Ionic 2 components

--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/teams/teams.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/teams/teams.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { LoadingController, NavController, NavParams } from 'ionic-angular';
+
+import * as _ from 'lodash';
 
 import { TeamHomePage } from '../pages';
 import { EliteApi } from '../../app/shared/shared';
@@ -9,17 +11,39 @@ import { EliteApi } from '../../app/shared/shared';
 })
 export class TeamsPage {
 
+  private allTeams: any; 
+  private allTeamDivisions: any;
   teams = [];
 
-  constructor(private nav: NavController,
-              private navParams: NavParams,
-              private eliteApi: EliteApi) {
+  constructor(
+      private loadingController: LoadingController,
+      private nav: NavController,
+      private navParams: NavParams,
+      private eliteApi: EliteApi) {
   }
 
   ionViewDidLoad() {
     let selectedTourney = this.navParams.data;
-    this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data => {
-      this.teams = data.teams;
+
+    let loader = this.loadingController.create({
+      content: 'Getting data...',
+    });
+
+    loader.present().then(() => {
+
+      this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data => {
+        this.allTeams = data.teams;
+        this.allTeamDivisions = 
+            _.chain(data.teams)
+            .groupBy('division')
+            .toPairs()
+            .map(item => _.zipObject(['divisionName', 'divisionTeams'], item))
+            .value();
+
+        this.teams = this.allTeamDivisions;
+        console.log('division teams', this.teams);
+        loader.dismiss();
+      });
     });
   }
 
