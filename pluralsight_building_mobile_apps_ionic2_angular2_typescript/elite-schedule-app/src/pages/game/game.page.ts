2556876f935a208782b1fe7b5d5056008ef245e1
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-30 16:17:23 -0200

    PluralSight - Ionic2 - EliteSchedule - Game - adds implementation with Advanced Ionic 2 Components

--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/game/game.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/elite-schedule-app/src/pages/game/game.page.ts
@@ -5,6 +5,7 @@ import { TeamHomePage } from '../pages';
 import { EliteApi } from '../../app/shared/shared';
 
 @Component({
+  selector: 'game-page',
   templateUrl: 'game.page.html'
 })
 export class GamePage {
@@ -18,12 +19,25 @@ export class GamePage {
 
   ionViewDidLoad() {
     this.game = this.navParams.data;
+    this.game.gameTime = Date.parse(this.game.time);
   }
 
   teamTapped(teamId) {
     let tourneyData = this.eliteApi.getCurrentTourney();
     let team = tourneyData.teams.find(t => t.id === teamId);
     this.nav.push(TeamHomePage, team);
+  }
+
+  goToDirections() {
+    // Placeholder
+  }
+
+  goToMap() {
+    // Placeholder
+  }
+
+  isWinner(score1, score2) {
+    return Number(score1) > Number(score2);
   }
 
 }
