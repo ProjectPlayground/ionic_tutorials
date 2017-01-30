beba936b30965aaa043757609e1d838b6861745a
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-30 18:38:07 -0200

    PluralSight - Ionic2 - Native - adds/sets 2 pages : Home and Vibration

---
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/vibration/vibration.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-vibration',
+  templateUrl: 'vibration.html'
+})
+export class Vibration {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
