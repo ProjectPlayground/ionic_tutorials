3ef91a34a13cedb6f386537a04d484468c464cd3
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-31 14:54:09 -0200

    PluralSight - Ionic2 - Native - use of 'cordova-plugin-device-motion' plugin

--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/device-motion/device-motion.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/device-motion/device-motion.page.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
 
+import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from 'ionic-native';
+
 @Component({
   templateUrl: 'device-motion.page.html',
 })
 export class DeviceMotionPage {
+  data: any;
+  subscription: any;
+
+  startWatching() {
+    var options: DeviceMotionAccelerometerOptions = {
+      frequency: 500,
+    };
+
+    this.subscription = DeviceMotion.watchAcceleration(options).subscribe((acceleration: DeviceMotionAccelerationData) => {
+      this.data = acceleration;
+    });
+  }
+
+  stopWatching() {
+    this.subscription.unsubscribe();
+  }
 
 }
