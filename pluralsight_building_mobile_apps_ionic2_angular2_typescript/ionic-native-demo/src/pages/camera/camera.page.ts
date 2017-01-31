c4d6117fbfdd5b9b0d5a03914881bf603fcf2c07
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-31 14:19:33 -0200

    PluralSight - Ionic2 - Native - use of 'cordova-plugin-camera' plugin

--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/camera/camera.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/camera/camera.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+import { Camera, CameraOptions } from 'ionic-native';
+
 @Component({
   templateUrl: 'camera.page.html',
 })
 export class CameraPage {
+
+  imgSrc: any = {};
+
+  takePicture() {
+    let options : CameraOptions = {
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      encodingType: Camera.EncodingType.PNG,
+      targetHeight: 500,
+      targetWidth: 500,
+      saveToPhotoAlbum: false,
+    };
+
+    Camera.getPicture(options).then((imageUri) => {
+      this.imgSrc = imageUri;
+    });
+  }
 
 }
