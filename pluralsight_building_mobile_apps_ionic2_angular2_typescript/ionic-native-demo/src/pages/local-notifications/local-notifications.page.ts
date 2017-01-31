6cd18780dc035248df0e9d863a43fcc65c142fed
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-31 14:54:53 -0200

    PluralSight - Ionic2 - Native - use of 'de.applant.cordova.plugin.local-notification' plugin

--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/local-notifications/local-notifications.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/local-notifications/local-notifications.page.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
 
+import { LocalNotifications } from 'ionic-native';
+
 @Component({
   templateUrl: 'local-notifications.page.html',
 })
 export class LocalNotificationsPage {
+  results: string;
+
+  constructor() {
+    LocalNotifications.on('click', notification => {
+      var data = JSON.parse(notification.data);
+      this.results = data.name;
+      alert(`Notification received! (${data.name})`);
+    });
+  }
+
+  scheduleNotification() {
+    LocalNotifications.schedule({
+      id: 1,
+      title: 'Cool Notification',
+      text: 'This is my cool notification!',
+      data: {
+        id: 21,
+        name: 'Cool Notification #21',
+      }
+    });
+  }
 
 }
