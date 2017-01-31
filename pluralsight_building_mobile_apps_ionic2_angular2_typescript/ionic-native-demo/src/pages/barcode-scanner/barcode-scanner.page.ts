38a040b11fb9567419b10d0107abd23dc7b9a3b5
Henrique Santos <henrique.santos@toutatisglobal.com>
2017-01-31 14:53:28 -0200

    PluralSight - Ionic2 - Native - use of 'phonegap-plugin-barcodescanner' plugin

--- pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/barcode-scanner/barcode-scanner.page.ts
+++ pluralsight_building_mobile_apps_ionic2_angular2_typescript/ionic-native-demo/src/pages/barcode-scanner/barcode-scanner.page.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
 
+import { BarcodeScanner } from 'ionic-native';
+
 @Component({
   templateUrl: 'barcode-scanner.page.html',
 })
 export class BarcodeScannerPage {
+  results: any;
+
+  scan() {
+    BarcodeScanner.scan().then((barcodeData) => {
+      // Success! Barcode data is here
+      this.results = barcodeData;
+    }, (err) => {
+      // An error ocurred
+      alert(`Error scanning: ${err}`);
+    });
+  }
+
+  reset() {
+    this.results = null;
+  }
+
+  lookup() {
+    window.open(`http://www.upcindex.com/${this.results.text}`, '_system');
+  }
 
 }
